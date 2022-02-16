75773097c0e0c63eba8704bf90d8e522dcd0726b
Michael Wiebusch <m.wiebusch@gsi.de>
2022-02-16 16:49:34 +0100

    board_misc.ino

--- pro_micro_spectral_pulser_v3.ino
+++ pro_micro_spectral_pulser_v3.ino
@@ -102,30 +102,6 @@ void display_status(int clear){
 
 
 
-void set_TX_LED(int state){
-  if(state > 0){
-    PORTD &= ~(1<<5);
-  } else {
-    PORTD |= 1<<5;  
-  }
-}
-
-void set_RX_LED(int state){
-  if(state > 0){
-    digitalWrite(RX_LED,0);
-  } else {
-    digitalWrite(RX_LED,1);
-  }
-}
-
-void set_USER_LED(int state){
-  if(state > 0){
-    digitalWrite(USER_LED,1);
-  } else {
-    digitalWrite(USER_LED,0);
-  }
-}
-
 
 void loop() {
     
