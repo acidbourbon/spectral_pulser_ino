f82955dd3a11c3c2abfaffc766d045cb42b30444
Michael Wiebusch <antiquark@gmx.net>
2022-02-16 22:59:56 +0100

    removed unnecessary refreshes of display, cleaned up the display state machine

--- pro_micro_spectral_pulser_v3.ino
+++ pro_micro_spectral_pulser_v3.ino
@@ -17,37 +17,37 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 
 void setup() {
-    // put your setup code here, to run once:
-    
-    
-    init_attenuator_pins();
-    init_pulser_pins();
-    init_tft_pins();
-    init_adc_aux_pins();
-    
-    
-    randomSeed(analogRead(0));
-    
-    
-    Serial.begin(115200);
-    
-    
-    
-    
-    tft.begin();
-    tft.setRotation(3);
-    
-    
-    tft.fillScreen(ILI9341_BLACK);
-    //testText();
-    //delay(1000);
-    
-    //demo_plot();
-    set_attenuator_dB(0);
-    
-    set_TX_LED(1);
-    set_RX_LED(1);
-    set_USER_LED(1);
+  // put your setup code here, to run once:
+  
+  
+  init_attenuator_pins();
+  init_pulser_pins();
+  init_tft_pins();
+  init_adc_aux_pins();
+  
+  
+  randomSeed(analogRead(0));
+  
+  
+  Serial.begin(115200);
+  
+  
+  
+  
+  tft.begin();
+  tft.setRotation(3);
+  
+  
+  tft.fillScreen(ILI9341_BLACK);
+  //testText();
+  //delay(1000);
+  
+  //demo_plot();
+  set_attenuator_dB(0);
+  
+  set_TX_LED(1);
+  set_RX_LED(1);
+  set_USER_LED(1);
 }
 
 
@@ -89,120 +89,114 @@ int scan_interval = scan_interval_fast;
 
 
 void loop() {
+  
+  // once every 4 ms
+  
+  
+  if(0){
+    int lines = 4;
+    int i = random(0,lines);
+    pulse_mv_combo(4*10*pow(2,i) ); // 10 because 20 db attenuator
+  }
+  
+  pulse_mv_combo(raw_amp_mv);
+  
+  loop_cnt = (loop_cnt+1)%scan_interval;
+  
+  if(loop_cnt == 0  ){
     
-    // once every 4 ms
     
     
-    if(0){
-        int lines = 4;
-        int i = random(0,lines);
-        pulse_mv_combo(4*10*pow(2,i) ); // 10 because 20 db attenuator
+    
+    before_pot_measurement();
+    // for the capacitors to recover
+    delay(20);
+    int rise_adc = meas_rise_adc();
+    int tail_adc = meas_tail_adc();
+    after_pot_measurement();
+    
+    att_pot = read_att_pot();
+    raw_amp_mv = 600.*(float(att_pot)/1023.) ;
+    
+    rise_pot = calc_rise_pot(rise_adc);
+    tail_pot = calc_tail_pot(tail_adc);
+    
+    
+    // has rise or tail pot turned?
+    if( (abs(rise_adc - last_rise_adc) > 2) or (abs(tail_adc - last_tail_adc) > 2) ){
+      tau_rise_ns = (rise_pot + R_SER_RISE)*C_RISE*1e9;
+      tau_tail_ns = (tail_pot + R_SER_TAIL)*C_TAIL*1e9;
+      
+      Q_pC = calc_Q_pC(raw_amp_mv,tau_tail_ns);
+      real_amp_mv = max_amplitude(Q_pC,tau_rise_ns,tau_tail_ns);
+      
+      last_rise_pot = rise_pot;
+      last_tail_pot = tail_pot;
+      last_rise_adc = rise_adc;
+      last_tail_adc = tail_adc;
+      
+      
+      
+      display_status(1); // update text and graph, but don't clear plot area
+      meta_loop_cnt = 0; // delay unconditional redraw
+      scan_interval = scan_interval_fast;
+      update_count_down = update_count_down_reset_val;
+      
+      // has attenuator poti turned?
+    } else if (abs(att_pot-last_att_pot)>2){
+      Q_pC = calc_Q_pC(raw_amp_mv,tau_tail_ns);
+      real_amp_mv = max_amplitude(Q_pC,tau_rise_ns,tau_tail_ns);
+      last_att_pot = att_pot;
+      display_status(10); // just update amplitude
+      meta_loop_cnt = 0; // delay unconditional redraw
+      scan_interval = scan_interval_fast;
+      update_count_down = update_count_down_reset_val;
+      
+    } else if (update_count_down > 0) {
+      // is it time for a final redraw
+      if (update_count_down == update_count_down_redraw_val) {
+        display_status(2); // clear plot area, redraw graph, update text
+        meta_loop_cnt = 0; // delay unconditional redraw
+      } else if (update_count_down == 1) {
+        // don't waste pulses - slower scanning for new input
+        scan_interval = scan_interval_slow;
+      }
+      // count down until you reach zero
+      update_count_down --;
     }
     
-    pulse_mv_combo(raw_amp_mv);
-    
-    loop_cnt = (loop_cnt+1)%scan_interval;
-    
-    if(loop_cnt == 0  ){
-        
-        
-        
-        
-        before_pot_measurement();
-        // for the capacitors to recover
-        delay(20);
-        int rise_adc = meas_rise_adc();
-        int tail_adc = meas_tail_adc();
-        after_pot_measurement();
-        
-        att_pot = read_att_pot();
-        raw_amp_mv = 600.*(float(att_pot)/1023.) ;
-        
-        rise_pot = calc_rise_pot(rise_adc);
-        tail_pot = calc_tail_pot(tail_adc);
-        
-        
-        // has rise or tail pot turned?
-        if( (abs(rise_adc - last_rise_adc) > 2) or (abs(tail_adc - last_tail_adc) > 2) ){
-            meta_loop_cnt = 0;
-            tau_rise_ns = (rise_pot + R_SER_RISE)*C_RISE*1e9;
-            tau_tail_ns = (tail_pot + R_SER_TAIL)*C_TAIL*1e9;
-            
-            Q_pC = calc_Q_pC(raw_amp_mv,tau_tail_ns);
-            real_amp_mv = max_amplitude(Q_pC,tau_rise_ns,tau_tail_ns);
-            
-            last_rise_pot = rise_pot;
-            last_tail_pot = tail_pot;
-            last_rise_adc = rise_adc;
-            last_tail_adc = tail_adc;
-            
-            
-            
-            display_status(1); // update text and graph, but don't clear plot area
-            scan_interval = scan_interval_fast;
-            update_count_down = update_count_down_reset_val;
-        
-        // has attenuator poti turned?
-        } else if (abs(att_pot-last_att_pot)>2){
-            Q_pC = calc_Q_pC(raw_amp_mv,tau_tail_ns);
-            real_amp_mv = max_amplitude(Q_pC,tau_rise_ns,tau_tail_ns);
-            last_att_pot = att_pot;
-            display_status(10); // just update amplitude
-            scan_interval = scan_interval_fast;
-            update_count_down = update_count_down_reset_val;
-            
-        // nothing has changed, maybe do an update after a while
-        } else {
-            if (update_count_down == 1) {
-                
-                //display_status(2);
-                update_count_down = 0;
-                scan_interval = scan_interval_slow;
-                
-            } else {
-                if (update_count_down == update_count_down_redraw_val) {
-                    display_status(2);
-                }
-                
-                if (update_count_down > 1) {
-                    update_count_down --;
-                }
-            }
-            
-        }
-
-        // for the capacitors to recover
-        delay(50);
-        
-        
-        
-        meta_loop_cnt = (meta_loop_cnt+1)%meta_loop_overflow;
-        if (meta_loop_cnt == 0){
-            // update battery reading 
-            display_status(0);
-        }
-    } 
+    // for the capacitors to recover
+    delay(50);
     
     
-    uint8_t buttons = decode_adc_buttons();  
-    uint8_t buttons_pressed = ~(last_buttons) & buttons; //detect rising edge
-    last_buttons = buttons;
     
-    if(buttons_pressed & (1<<3)){
-      toggle_TX_LED();  
+    meta_loop_cnt = (meta_loop_cnt+1)%meta_loop_overflow;
+    if (meta_loop_cnt == 0){
+      // update battery reading 
+      display_status(0);
     }
-    if(buttons_pressed & (1<<2)){
-      toggle_RX_LED();  
-    }
-    if(buttons_pressed & (1<<1)){
-      toggle_USER_LED();  
-    }
-    
-//     set_USER_LED( buttons & (1<<3) );
-//     set_TX_LED( buttons & (1<<2) );
-//     set_RX_LED( buttons & (1<<1) );
-    
-    
-    
-    
+  } 
+  
+  
+  uint8_t buttons = decode_adc_buttons();  
+  uint8_t buttons_pressed = ~(last_buttons) & buttons; //detect rising edge
+  last_buttons = buttons;
+  
+  if(buttons_pressed & (1<<3)){
+    toggle_TX_LED();  
+  }
+  if(buttons_pressed & (1<<2)){
+    toggle_RX_LED();  
+  }
+  if(buttons_pressed & (1<<1)){
+    toggle_USER_LED();  
+  }
+  
+  //     set_USER_LED( buttons & (1<<3) );
+  //     set_TX_LED( buttons & (1<<2) );
+  //     set_RX_LED( buttons & (1<<1) );
+  
+  
+  
+  
 }
