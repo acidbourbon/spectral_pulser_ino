cbeee915e9e6db35070f0e406bd7148a93579490
Michael Wiebusch <m.wiebusch@gsi.de>
2022-02-18 15:58:01 +0100

    paramentrized attenuator things with defines

--- attenuator.ino
+++ attenuator.ino
@@ -12,6 +12,12 @@ void init_attenuator_pins(){
     
 }
 
+#define ATT_CLK_PORT PORTC
+#define ATT_SDI_PORT PORTD
+#define ATT_LE_PORT  PORTE
+#define ATT_CLK_PIN 6
+#define ATT_SDI_PIN 7
+#define ATT_LE_PIN  6
 
 void set_attenuator(int val){
   
@@ -21,17 +27,17 @@ void set_attenuator(int val){
 
   // speeding it up by fast commands
   for (int i = 5; i >=0 ; i--){
-    PORTC &= ~(1<<6); //digitalWrite(ATT_CLK, 0);
+    ATT_CLK_PORT &= ~(1<<ATT_CLK_PIN); //digitalWrite(ATT_CLK, 0);
    
-    PORTD &= ~(1<<7);
-    PORTD |= ((val&(1<<i))>>i)<<7; //digitalWrite(ATT_SDI, (val&(1<<i))>>i );
+    ATT_SDI_PORT &= ~(1<<ATT_SDI_PIN);
+    ATT_SDI_PORT |= ((val&(1<<i))>>i)<<ATT_SDI_PIN; //digitalWrite(ATT_SDI, (val&(1<<i))>>i );
     
-    PORTC |= (1<<6); //digitalWrite(ATT_CLK, 1);
+    ATT_CLK_PORT |= (1<<ATT_CLK_PIN); //digitalWrite(ATT_CLK, 1);
   }
-    PORTC &= ~(1<<6); //digitalWrite(ATT_CLK, 0);
-    PORTE |=  (1<<6); //digitalWrite(ATT_LE,  1);
-    PORTC |=  (1<<6); //digitalWrite(ATT_CLK, 1);
-    PORTE &= ~(1<<6); //digitalWrite(ATT_LE,  0);
+    ATT_CLK_PORT &= ~(1<<ATT_CLK_PIN); //digitalWrite(ATT_CLK, 0);
+    ATT_LE_PORT |=  (1<<ATT_LE_PIN); //digitalWrite(ATT_LE,  1);
+    ATT_CLK_PORT |=  (1<<ATT_CLK_PIN); //digitalWrite(ATT_CLK, 1);
+    ATT_LE_PORT &= ~(1<<ATT_LE_PIN); //digitalWrite(ATT_LE,  0);
     
     // // with arduino functions
     //   for (int i = 5; i >=0 ; i--){
